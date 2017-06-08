333292f88e9ca3470c22ae44d0c16e37b5a6e00b
vrunda2712 <angy.v27@gmail.com>
2017-06-08 14:06:01 -0400

    Define dynamic routes for kitten

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,9 @@ class PagesController < ApplicationController
   def contest
     @header = "Routing Controller Intro - Contest Page"
   end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
