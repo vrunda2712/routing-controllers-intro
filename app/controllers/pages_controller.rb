c00d243ad5ab4b7a4fd0d1135ebbb28190868a03
vrunda2712 <angy.v27@gmail.com>
2017-06-08 13:58:03 -0400

    Use of instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,14 @@ class PagesController < ApplicationController
 
   def welcome
     # render :welcome
+    @header = "Routing Controller Intro - Welcome Page"
   end
 
   def about
-
+    @header = "Routing Controller Intro - About Page"
   end
 
   def contest
-
+    @header = "Routing Controller Intro - Contest Page"
   end
 end
